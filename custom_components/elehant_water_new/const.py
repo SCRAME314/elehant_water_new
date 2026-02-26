e8b4fc1147f5d19c64ddcfbb47ab82c31723b2ac
SCRAME314 <68689925+SCRAME314@users.noreply.github.com>
2026-02-26 13:13:32 +0300

    Update const.py новая версия

--- custom_components/elehant_water_new/const.py
+++ custom_components/elehant_water_new/const.py
@@ -1,39 +1,60 @@
-"""Constants for Elehant Water integration."""
-from homeassistant.const import Platform
+"""Constants for the Elehant Water integration."""
 
 DOMAIN = "elehant_water_new"
-PLATFORMS = [Platform.SENSOR]
 
-# Configuration
-CONF_DEVICES = "devices"
-CONF_MEASUREMENT_WATER = "measurement_water"
-CONF_MEASUREMENT_GAS = "measurement_gas"
-CONF_SCAN_DURATION = "scan_duration"
+# Конфигурация
+CONF_COUNTERS = "counters"
+CONF_COUNTER_ID = "counter_id"
+CONF_COUNTER_TYPE = "counter_type"
+CONF_NAME = "name"
+
+# Типы счетчиков
+COUNTER_TYPE_WATER = "water"
+COUNTER_TYPE_GAS = "gas"
+
+# Опции
 CONF_SCAN_INTERVAL = "scan_interval"
-CONF_WATER_TYPE = "water_type"
-CONF_NAME_TEMP = "name_temp"
+DEFAULT_SCAN_INTERVAL = 60
 
-# Default values
-DEFAULT_SCAN_DURATION = 10
-DEFAULT_SCAN_INTERVAL = 600
-DEFAULT_MEASUREMENT_WATER = "m3"
-DEFAULT_MEASUREMENT_GAS = "m3"
+# MAC-маски для определения производителя и типа устройства
+# Счетчики воды СГБТ (газ?) / Возможно газовые счетчики
+MAC_MASK_GAS = [
+    'b0:10:01',
+    'b0:11:01',
+    'b0:12:01',
+    'b0:32:01',
+    'b0:42:01'
+]
 
-# Device types
-DEVICE_TYPE_WATER = "water"
-DEVICE_TYPE_GAS = "gas"
+# Счетчики воды СВД (с температурой)
+MAC_MASK_WATER_TEMP = [
+    'b0:01:02',
+    'b0:02:02'
+]
 
-# Water types
-WATER_TYPE_HOT = "hot"
-WATER_TYPE_COLD = "cold"
+# Счетчики воды СВТ (двухтарифные)
+MAC_MASK_WATER_DUAL = [
+    'b0:03:02',
+    'b0:04:02',
+    'b0:05:02',
+    'b0:06:02'
+]
 
-# Measurement units
-MEASUREMENT_LITERS = "l"
-MEASUREMENT_CUBIC_METERS = "m3"
+# Все маски для воды (для удобства проверки)
+MAC_MASK_WATER = MAC_MASK_WATER_TEMP + MAC_MASK_WATER_DUAL
 
-# Attributes
+# Ключи для хранения данных в hass.data
+DATA_COORDINATOR = "coordinator"
+DATA_CONFIG = "config"
+DATA_DEVICES = "devices"
+DATA_SCANNER = "scanner"
+
+# Атрибуты сенсоров
 ATTR_BATTERY_LEVEL = "battery_level"
 ATTR_RSSI = "rssi"
-ATTR_TEMPERATURE = "temperature"
+ATTR_COUNTER_TYPE = "counter_type"
 ATTR_LAST_SEEN = "last_seen"
-ATTR_COUNTER_VALUE = "counter_value"
+ATTR_TEMPERATURE = "temperature"  # для СВД
+ATTR_TARIFF_1 = "tariff_1"  # для двухтарифных
+ATTR_TARIFF_2 = "tariff_2"  # для двухтарифных
+ATTR_CURRENT_TARIFF = "current_tariff"  # текущий активный тариф
